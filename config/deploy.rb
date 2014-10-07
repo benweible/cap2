89f750d11ddb32dcb0b6ce3b061eba9612673a74
Ben Weible <benweible@yahoo.com>
2014-10-07 14:35:59 -0700

    pass chng

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,24 +45,8 @@ set :ssh_options, { :forward_agent => true, :port => 22 }
 set :keep_releases, 3
 
 namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
   end
-
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
 end
